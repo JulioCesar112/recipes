1f93b261c0f8578a64879ade0c9092d9142b7b9d
Julio Cesar <billyjoplin112@gmail.com>
2022-11-03 21:20:14 -0600

    first commit

---
+++ src/models/initModels.js
@@ -0,0 +1,37 @@
+const Users = require('./users.models')
+const Categories = require('./categories.models')
+const Ingredients = require('./ingredients.models')
+const Instructions = require('./instructions.models')
+const RecipesIngredients = require('./recipes_ingredients.models')
+const Recipes = require('./recipes.models')
+const Types = require('./types.models')
+const UsersIngredients = require('./users_ingredients.models')
+const UsersRecipes = require('./users_recipes.models')
+
+const initModels = () => {
+
+    //* Users 1:M Recipes
+    Users.hasMany(Recipes)
+    Recipes.belongsTo(Users)
+
+    //* Users 1:M UserRecipes
+    Users.hasMany(UsersRecipes)
+    UsersRecipes.belongsTo(Users)
+
+    //* Recipes 1:M UserRecipes
+    Recipes.hasMany(UsersRecipes)
+    UsersRecipes.belongsTo(Recipes)
+
+    //* Users 1:M UserIngredients
+    Users.hasMany(UsersIngredients)
+    UsersIngredients.belongsTo(Users)
+
+    //* Ingredients 1:M UserIngredients
+    Ingredients.hasMany(UsersIngredients)
+    UsersIngredients.belongsTo(Ingredients)
+
+
+}
+
+
+module.exports = initModels
